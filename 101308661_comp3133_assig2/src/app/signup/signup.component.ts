ecbede2efb9a1c0c449cfbdce22d5a5adf7e1772
HamdaanYare <fanahyare114@gmail.com>
2023-03-23 14:45:46 -0500

    added some css to signup

--- 101308661_comp3133_assig2/src/app/signup/signup.component.ts
+++ 101308661_comp3133_assig2/src/app/signup/signup.component.ts
@@ -1,30 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
   title = 'Sign Up';
-  signupForm: FormGroup | undefined;
-
-  ngOnInit() {
-    this.signupForm = new FormGroup({
-      first_name: new FormControl('', Validators.required),
-      last_name: new FormControl('', Validators.required),
-      gender: new FormControl('', Validators.required),
-      salary: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required),
-      confirm_password: new FormControl('', Validators.required)
-    });
-  }
+  firstName: string = "";
+  lastName: string = "";
+  gender: string = "";
+  salary: number = 0;
+  password: string = "";
+  email: string = "";
 
   handleSignup() {
-    // console.log(this.signupForm.value);
-    
-    // TODO: Send the form data to a backend API
+      console.log("Email: " + this.email + " Password: " + this.password);
   }
 }
