08146b2a0f5039cb12242981fab951e8497cc571
HamdaanYare <fanahyare114@gmail.com>
2023-03-23 14:15:53 -0500

    added fuctuonality the signup

--- 101308661_comp3133_assig2/src/app/signup/signup.component.ts
+++ 101308661_comp3133_assig2/src/app/signup/signup.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-  // Add any class properties here
+export class SignupComponent implements OnInit {
+  title = 'Sign Up';
+  signupForm: FormGroup | undefined;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      first_name: new FormControl('', Validators.required),
+      last_name: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      salary: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirm_password: new FormControl('', Validators.required)
+    });
+  }
+
+  handleSignup() {
+    // console.log(this.signupForm.value);
+    
+    // TODO: Send the form data to a backend API
+  }
 }
