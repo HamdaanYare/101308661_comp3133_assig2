3dccb2bdcc48c0f13c2cee7e76129d318d0310be
HamdaanYare <fanahyare114@gmail.com>
2023-04-05 01:14:52 -0500

    added add-employee

--- 101308661_comp3133_assig2/src/app/add-employee/add-employee.component.spec.ts
+++ 101308661_comp3133_assig2/src/app/add-employee/add-employee.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ButtonComponent } from './button.component';
+import { AddEmployeeComponent } from './add-employee.component';
 
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe('AddEmployeeComponent', () => {
+  let component: AddEmployeeComponent;
+  let fixture: ComponentFixture<AddEmployeeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ AddEmployeeComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(AddEmployeeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
